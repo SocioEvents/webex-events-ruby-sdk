b09915b16e266defd662210eb596ea017226df6e
Mehmet Yıldız <yildiz@cisco.com>
2023-11-25 12:58:37 +0300

    remove unnecessary spec.

--- spec/webex/events_spec.rb
+++ spec/webex/events_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Webex::Events do
   it 'has a version number' do
     expect(Webex::Events::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
