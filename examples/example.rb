9cc1737382ba2ae8eecf3f3c357a0114806b61e4
Mehmet Yıldız <yildiz@cisco.com>
2023-12-04 08:51:01 +0300

    add examples.

---
+++ examples/example.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative '../lib/webex/events'
+
+def mutation_query
+  query = <<-GQL
+    mutation TrackDelete($input: TrackDeleteInput!) {
+      trackDelete(input: $input) {
+        success
+      }
+    }
+  GQL
+  variables = {
+    input: { ids: [1], eventId: 1 }
+  }
+  response = Webex::Client.query(query: query, operation_name: 'TrackDelete', variables: variables, headers: { 'Idempotency-Key' => SecureRandom.uuid })
+  pp response
+end
+
+def single_query
+  query = <<-GQL
+    query Currency($isoCode: String!) {
+      currency(isoCode: $isoCode) {
+        isoCode
+        name
+      }
+    }
+  GQL
+  response = Webex::Client.query(query: query, operation_name: 'Currency', variables: { isoCode: 'USD' }, headers: {})
+  pp response
+end
+
+def get_connection
+  query = <<-GQL
+      fragment EventStuff on Event {
+          id
+          name
+      }
+      query EventsConnection($first: Int){
+          eventsConnection(first: $first) {
+              edges{
+                  cursor
+                  node{
+                      ...EventStuff
+                  }
+              }
+          }
+      }
+  GQL
+
+  response = Webex::Client.query(query: query, operation_name: 'EventsConnection', variables: { first: 10 }, headers: {})
+  pp response
+end
